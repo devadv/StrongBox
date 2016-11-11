4b5f398ae25fe1537a756ff7796bd88a55818052
devadv <ansemsdevries@gmail.com>
2016-11-11 23:09:28 +0100

    added jasypt lib and tested encryption
    Read TODO for details about Unlimited Strength Policy

--- src/strongbox/test/MainTest.java
+++ src/strongbox/test/MainTest.java
@@ -1,10 +1,20 @@
 package strongbox.test;
 
+import strongbox.test.encryption.EncryptingText;
+
 public class MainTest{
 	
 	
 	public static void main(String[] args) {
 		System.out.println("StrongBox Development started");
+		System.out.println("======Starting Encryption=======");
+		String s = "Example text to be encrypted";
+		System.out.println("encrytion text: " + s);
+		String eText = EncryptingText.encrypt(s);
+		System.out.println("Encrypted text will be decrypted: " + eText );
+		System.out.println("======Starting Decryption=======");
+		System.out.println("Decrypt: " + EncryptingText.decrypt(eText));
+		
 	}
 	
 	
---
+++ src/strongbox/test/encryption/EncryptingText.java
@@ -0,0 +1,22 @@
+package strongbox.test.encryption;
+
+
+import org.jasypt.util.text.StrongTextEncryptor;
+
+public class EncryptingText {
+	
+
+	public static String encrypt(String text) {
+		StrongTextEncryptor sTextEncryptor = new StrongTextEncryptor();
+		sTextEncryptor.setPassword("eensterkwachtwoord");
+		String encryptedText = sTextEncryptor.encrypt(text);
+		return encryptedText;
+	}
+	public static String decrypt(String encryptedtext){
+		StrongTextEncryptor sTextEncryptor = new StrongTextEncryptor();
+		sTextEncryptor.setPassword("eensterkwachtwoord");
+		String decryptedText =sTextEncryptor.decrypt(encryptedtext);
+		return decryptedText;
+		
+	}
+}
