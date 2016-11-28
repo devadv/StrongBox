498603d0260bdf421745fa200d77c36ffa810868
devadv <ansemsdevries@gmail.com>
2016-11-28 21:38:57 +0100

    added Login

--- src/strongbox/test/login/Controller.java
+++ src/strongbox/test/login/Controller.java
@@ -1,11 +1,10 @@
 package strongbox.test.login;
 
-import org.jasypt.util.password.StrongPasswordEncryptor;
+
 
 public class Controller {
 
 		
-
 	public static void main(String[] args) {
 
 		ConfigProp cProp = new ConfigProp();
--- src/strongbox/test/login/Login.java
+++ src/strongbox/test/login/Login.java
@@ -1,49 +1,60 @@
 package strongbox.test.login;
 
-import java.awt.BorderLayout;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class LoginPanel extends JPanel implements ActionListener{
+import strongbox.view.GUI;
+
+public class Login extends JPanel implements ActionListener{
 	
 	
-	private JTextField usernameField;
+	
 	private JTextField passwordField;
+	private JFrame frame;
+	private ConfigProp cProp;
 
-	public LoginPanel() {
-
-		JLabel labelUsername = new JLabel("Login: ");
+	public Login(ConfigProp cProp) {
+		this.cProp = cProp;
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 70);
+		frame.setLocation(600, 400);
 		JLabel labelPassword = new JLabel("Password: ");
-		usernameField = new JTextField(10);
-		passwordField = new JTextField(10);
+		passwordField = new JPasswordField(10);
+		passwordField.addActionListener(this);
 		JButton button = new JButton("Login");
 		button.addActionListener(this);
 		setLayout(null);
-		labelUsername.setBounds(10,10 ,120,20);
-		labelPassword.setBounds(10,40,120,20);
-		usernameField.setBounds(110,10,150,20);
-		passwordField.setBounds(110,40,150,20);
-		button.setBounds(200,70,80,20);
-		add(labelUsername);
-		add(usernameField);
+		labelPassword.setBounds(10,10,120,20);
+		passwordField.setBounds(110,10,150,20);
+		button.setBounds(200,40,80,20);
 		add(labelPassword);
 		add(passwordField);
 		add(button);
-		
+		frame.add(this);
+		frame.setVisible(true);
 		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("clicked!");
-		System.out.println(usernameField.getText());
-		System.out.println(passwordField.getText());
+		
+		if(cProp.setMasterKeyAndLogin(passwordField.getText())){
+			System.out.println("Login successful");
+			frame.dispose();
+			new GUI();
+		}else{
+			frame.dispose();
+		}
+		
 		
 	}
 
